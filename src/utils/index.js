a76f6e452bbf6f73b3361f3e780a6b39cacbbf07
samira rezaei <samirarezai1996@gmail.com>
2021-08-25 11:06:13 +0430

    process auction

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,4 +5,5 @@ export const BASE_URL = "https://api.beta.smartauctionhouse.com/api";
 // export const BASE_URL = "http://192.168.0.182:9000/api";
 // export const BASE_URL = "http://192.168.0.182:9001/api";
 
+
 export const COOKIE_EXPIRES = 1;
