b14b531bc41c9b217389e025162b03b8e4efb838
reza semyari <rezasemyari1991@gmail.com>
2021-10-27 18:37:35 +0330

    fix bug in ticket page

--- src/Views/TicketPage/TicketBoxAdmin.jsx
+++ src/Views/TicketPage/TicketBoxAdmin.jsx
@@ -17,7 +17,7 @@ function TicketBoxAdmin(props) {
                             <div className="d-flex mb-2">
                                 <img src={dark_Profiel_icon} alt="icon-profile"/>
                                 <div className="d-block">
-                                    <p className=" mb-0 mr-1 mt-1 text-right">{`${props.ticket?.owner?.last_name} ${' '} ${props.ticket?.owner?.first_name}`}</p>
+                                    <p className=" mb-0 mr-1 mt-1 text-right">{`${props.ticket?.owner?.first_name} ${' '} ${props.ticket?.owner?.last_name}`}</p>
                                     <p className="d-sm-none  mb-0 mr-1 text-right">{props.ticket.created_at ? `${momentJalaali(props.ticket.created_at).format(`HH:mm  -   jYYYY/jMM/jDD`)}`: ''}</p>
                                 </div>
                             </div>
