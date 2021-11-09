c91a42a07be4a8c8ad5934781979c39c293f8a44
reza semyari <rezasemyari1991@gmail.com>
2021-11-09 13:03:00 +0330

    add config in index.js & package.json for handle change url

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,29 @@
-export const BASE_URL = "https://api.beta.smartauctionhouse.com/api";
-// export const BASE_URL = "http://backend.auction.amln.ir/api";
-// export const BASE_URL = "http://192.168.0.38:8000/api";
-// export const BASE_URL = "http://192.168.0.140:8003/api";
-// export const BASE_URL = "http://192.168.0.182:9000/api";
-// export const BASE_URL = "http://192.168.0.182:9002/api";
-
-
-export const COOKIE_EXPIRES = 1;
+const dev = {
+    BASE_URL : "http://192.168.0.182:9002/api",
+    // PARAMS:
+    OOKIE_EXPIRES : 1 ,
+  }
+  
+  const test = {
+    BASE_URL : "https://api.beta.smartauctionhouse.com/api",
+      // PARAMS:
+    COOKIE_EXPIRES : 1 ,
+  }
+  
+  const prod = {
+    BASE_URL : "https://api.beta.smartauctionhouse.com/api",
+      // PARAMS:
+    COOKIE_EXPIRES : 1 ,
+  }
+  
+  let config = dev;
+  
+  if(process.env.REACT_APP_STAGE === 'production'){
+     config =  prod ;
+  
+  }else if(process.env.REACT_APP_STAGE === 'test'){
+     config = test ;
+  
+  }
+  
+  Object.assign(module.exports , config)
