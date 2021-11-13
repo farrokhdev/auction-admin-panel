21a26dfdb22be078cfe059ed413eba0d751c4829
reza semyari <rezasemyari1991@gmail.com>
2021-11-13 18:15:59 +0330

    add multiple upload images in create artwork

--- src/Views/AddNewArtwork/AddNewArtwork.jsx
+++ src/Views/AddNewArtwork/AddNewArtwork.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ImgCrop from 'antd-img-crop';
 import { Form, Input, InputNumber, Button, Space, Breadcrumb, Select, notification, Alert, Spin , Upload , message} from 'antd';
-import {PictureOutlined , StarOutlined} from "@ant-design/icons";
+import {PictureOutlined , DeleteOutlined} from "@ant-design/icons";
 import { NavLink, Link } from 'react-router-dom';
 import { BASE_URL } from '../../utils';
 import {PRE_UPLOAD} from '../../utils/constant';
@@ -36,14 +36,13 @@ function AddNewArtwork(props) {
     const [houseAuctionsList, setHouseAuctionsList] = useState([])
 
     const [CoreUpload, setCoreUpload] = useState([]);
-    const [Uploaded, setUploaded] = useState(false);
     const [Uploading, setUploading] = useState(false);
     const [media, setMedia] = useState(null)
     const [is_upload, setIs_upload] = useState(true)
 
     const [uploadList, setUploadList] = useState([])
-    const [newArtwork, setNewArtwork] = useState({ category_id : []})
-
+    // const [newArtwork, setNewArtwork] = useState({ category_id : []})
+    const [statusDefaultImage, setStatusDefaultImage] = useState(false)
     const [minPrice, setMinPrice] = useState()
 
     const { Option } = Select;
@@ -93,13 +92,12 @@ function AddNewArtwork(props) {
 
     })
 
-    
 
 
     const onFinish = (values) => {
         setLoading(true)
 
-        if (media === null) {
+        if (!uploadList.length) {
             setIs_upload(false)
         }
 
@@ -121,15 +119,6 @@ function AddNewArtwork(props) {
             "persion_description": values.persion_description,
             "english_description": values.english_description,
             "media": uploadList,
-            
-            // [{
-            //     "media_path": is_upload ? CoreUpload.upload_url : "",
-            //     "type": "image",
-            //     "bucket_name": "image",
-            //     "file_key": is_upload ? CoreUpload.file_key : "",
-            //     "is_default" : false
-            // }],
-            // "media": media,
             "price": values.price,
             "price_max": values.price_max,
             "price_min": values.price_min,
@@ -160,13 +149,6 @@ function AddNewArtwork(props) {
     };
 
 
-    const handleResultUpload = (value) => {
-        if (value?.media_path)
-            setMedia(value)
-        // dispatch(setAUCTION({media:value}))
-    }
-
-
     const propsUpload = {
         listType: "picture",
     
@@ -175,13 +157,15 @@ function AddNewArtwork(props) {
           if (status !== "uploading") {
     
           }
-          if (status === "done" && !!uploadList.url) {
-              console.log("info_file --->>" , info.file);
-            setUploadList([...uploadList , info.file])
-            message.success(`${info.file.name} با موفقیت آپلود شد.`);
+          if (status === "done" ) {
+       
           } else if (status === "error") {
-            message.error(`آپلود ${info.file.name} با خطا مواجه شد.`);
+                info.fileList.filter(item => item.uid !== info.file.uid)
+                setUploadList(uploadList.filter(item => item.uid !== info.file.uid));
+
           }
+
+          return info
     
         },
         progress: {
@@ -189,21 +173,49 @@ function AddNewArtwork(props) {
               '0%': '#e6007e',
               '100%': '#e6007e',
             },
+            width : '50%',
             strokeWidth: 3,
             format: percent => `${parseFloat(percent.toFixed(2))}%`,
+          },
+
+
+          onRemove : (file)=> {
+            setUploadList(uploadList.filter(item => item.uid !== file.uid));
           },
 
           showUploadList: {
             showDownloadIcon: false,
             downloadIcon: 'download ',
             showRemoveIcon: true,
-            // removeIcon: <StarOutlined onClick={e => console.log(e, 'custom removeIcon event')} />,
-            removeIcon: <input type="checkbox"/>,
+            // removeIcon: <DeleteOutlined onClick={e => handleRemoveUploadImage(e)} />,
           },
-        
-    
+
+          itemRender : (OriginNode , file , fileList , actions ) => {
+
+            return (
+      
+                [OriginNode , 
+                    <div  className="default-checkbox-image-upload d-flex justify-content-end" >
+                        <input onClick={(e )=>defaultImageHandler(e , file.uid)} name="is_default" className="ml-1  mt-2" type="radio"/>
+                        <span className="pb-2 ml-2">پیش‌فرض</span>
+                    </div>
+                ]
+            )
+          }
+
       };
 
+
+    const defaultImageHandler = (e , uid) => {
+
+       let newList =  uploadList?.map(item => item.uid === uid  ? {...item , is_default : true} : {...item , is_default : false})
+
+        setUploadList(newList)
+
+    }
+
+
+  
 
       console.log("uploadList ---->" , uploadList);
 
@@ -268,9 +280,6 @@ function AddNewArtwork(props) {
                                         <Dragger
                                                 {...propsUpload}
                                                 className="upload-list-inline"
-                                                // maxCount={1}
-                                                //   onRemove={()=>setUploadList([...uploadList])}
-                                                  onRemove={()=>alert('delete')}
                                                 customRequest={async (e) => {
                                                     const { file, onSuccess, onError } = e;
 
@@ -278,39 +287,38 @@ function AddNewArtwork(props) {
                                                     .post(`${BASE_URL}${PRE_UPLOAD}`, {
                                                         content_type: "image",
                                                     }).then((res) => {
-                                                        onSuccess({ status: "success" });
-                                                        setUploadList([
-                                                            ...uploadList , 
-                                                            { 
+                                                        onSuccess(
+                                                            { status: "success" }
+                                                        );
+
+                                                        let uploadImage;
+                                                        uploadImage = { 
                                                                 file_key : res.data.data.result.file_key,
                                                                 media_path : res.data.data.result.upload_url,
                                                                 type : "image",
                                                                 bucket_name : "image",
-                                                                is_default : false
+                                                                is_default : false,
+                                                                uid : file.uid
                                                             }
-                                                        ])
-
-                                                        setNewArtwork({
-                                                        ...newArtwork,
-                                                        
-                                                        });
 
                                                         if (
-                                                        res.data.data.result.upload_url &&
-                                                        file?.type.split("/")[0] === "image"
+                                                            res.data.data.result.upload_url &&
+                                                            file?.type.split("/")[0] === "image"
                                                         ) {
 
-                                                        UploadAxios.put(res.data.data.result.upload_url, file)
-                                                        .then((res) => {
+                                                        UploadAxios.put(res.data.data.result.upload_url, file).then((res) => {
 
 
-                                      
-
+                                                            setUploadList([...uploadList ,  uploadImage])
+                                                                message.success(` با موفقیت بازگذاری شد.`);
                                                             }).catch((err) => {
-                                                            console.error(err);
+                                                                console.error(err);
+                                                                onError({ status: "error" });
+                                                                message.error(`بارگذاری  با خطا مواجه شد.`);
                                                             });
+
                                                         } else {
-                                                            // setImageList([]);
+                                                           
                                                         }
                                                     })
                                                     .catch((err) => {
